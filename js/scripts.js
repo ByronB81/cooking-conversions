6a26bfb6cf36247bc5077c3e6f8bb60391525329
byron blocker and Emily Dokken <emily@>
2017-02-15 20:39:32 -0800

    built cooking conversion

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Business Logic
+var convert = function(numberOfCups) {
+  // console.log(cups);
+	return numberOfCups * 16;
+};
+
+
+// Front-End Logic
+$(document).ready(function(){
+  $("#blanks form").submit(function(event) {
+    event.preventDefault();
+    var cupsInput = $("input#cups").val();
+    var result = convert(cupsInput);
+    // console.log(result);
+    $(".solution").text(result);
+  });
+});
